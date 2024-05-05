954ff2854af316d754b99e3eb4e602c4a7e3713c
AkagiYuu <khanghuynh0245@gmail.com>
2024-05-05 22:57:12 +0700

    feat: new error type

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,35 @@
-pub mod router;
-pub mod controller;
-pub mod middleware;
-pub mod util;
-pub mod model;
+#![feature(const_option)]
+#![feature(const_int_from_str)]
+#![feature(iter_array_chunks)]
+#![feature(custom_test_frameworks)]
+
+pub mod api;
+pub mod app_state;
+pub mod config;
+pub mod database;
 pub mod enums;
+pub mod error;
+pub mod util;
+
+pub use error::{Error, Result};
 
 #[macro_use]
 extern crate lazy_static;
-use anyhow::Result;
+
+use std::{net::SocketAddr, sync::Arc};
 use tokio::net::TcpListener;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-use util::{app_state::AppState, keys::Keys};
-use dotenvy_macro::dotenv;
 
-lazy_static! {
-    static ref KEYS: Keys = Keys::new(dotenv!("JWT_SECRET").as_bytes());
-}
-
+use app_state::AppState;
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> anyhow::Result<()> {
     tracing_subscriber::registry()
         .with(tracing_subscriber::fmt::layer())
         .init();
-    let state = AppState::new().await?;
-    let app = router::build(state);
-    let listener = TcpListener::bind("0.0.0.0:3000").await?;
+    let state = Arc::new(AppState::new().await?);
+    let app = api::router::build(state);
+    let listener = TcpListener::bind(SocketAddr::new([0, 0, 0, 0].into(), config::PORT)).await?;
     axum::serve(listener, app).await?;
 
     Ok(())
