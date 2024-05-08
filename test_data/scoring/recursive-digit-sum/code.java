598da0a49147fb895540736baa54eb2538d899d1
AkagiYuu <khanghuynh0245@gmail.com>
2024-05-08 23:56:55 +0700

    feat(util/scoring): add more unit test

---
+++ test_data/scoring/recursive-digit-sum/code.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'superDigit' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING n
+     *  2. INTEGER k
+     */
+
+    public static int superDigit(String n, int k) {
+        long sum = 0;
+        for(int i = 0; i < n.length(); i++){
+            sum += n.charAt(i) - '0';
+        }
+        sum *= k;
+        return division(sum);
+    }
+
+    public static int division(long n){
+        if(n < 10){
+            return (int) n;
+        }
+        else {
+            long sum = 0;
+            while (n > 0){
+                sum += n % 10;
+                n /= 10;
+            }
+
+            return division(sum);
+        }
+
+    }
+
+
+}
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        String n = firstMultipleInput[0];
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = Result.superDigit(n, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+
