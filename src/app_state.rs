42dcc89695af06226d8223fa86c7c2d9430a427e
AkagiYuu <khanghuynh0245@gmail.com>
2024-05-13 08:03:17 +0700

    feat: add /auth/session/socket

--- src/app_state.rs
+++ src/app_state.rs
@@ -1,16 +1,24 @@
+use std::collections::HashSet;
+use std::sync::Mutex;
+
 use anyhow::Result;
 use sqlx::PgPool;
+use uuid::Uuid;
 
 use crate::config;
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct AppState {
     pub database: PgPool,
+    pub logged_in_account_ids: Mutex<HashSet<Uuid>>,
 }
 impl AppState {
     pub async fn new() -> Result<Self> {
         let pool = PgPool::connect(config::DATABASE_URL).await?;
 
-        Ok(Self { database: pool })
+        Ok(Self {
+            database: pool,
+            logged_in_account_ids: Mutex::new(HashSet::new()),
+        })
     }
 }
