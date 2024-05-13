4c30c0605154d58f731cafee0b2cd3fde4bb3a69
LeviathanST <phamquochung170505@gmail.com>
2024-05-13 12:00:49 +0700

    FIX: remove check account

--- src/api/controller/team/join.rs
+++ src/api/controller/team/join.rs
@@ -10,57 +10,47 @@ use std::option::Option;
 use crate::{
     api::extractor::JWTClaims,
     app_state::AppState,
-    database::model::{Account, Member, Room},
-    enums::AccountRole,
-    error::Error,
+    database::model::{Member, Room},
     util::team::JoinResult,
-    Result,
+    Error, Result,
 };
 
 #[derive(Debug, Deserialize)]
 pub struct JoinRoomInfo {
     #[serde(rename = "roomId")]
-    room_id: String,
+    room_id: i32,
     #[serde(rename = "code")]
     room_code: Option<String>,
 }
 
 #[debug_handler]
 pub async fn join(
-    jwt_claims: JWTClaims,
     State(state): State<Arc<AppState>>,
+    jwt_claims: JWTClaims,
     Json(join_room_info): Json<JoinRoomInfo>,
 ) -> Result<Json<JoinResult>> {
-    let account = Account::get_one_by_id(jwt_claims.sub, &state.database)
+    let member: Member = Member::get_one_by_account_id(jwt_claims.sub, &state.database)
         .await
         .map_err(|err| Error::Unauthorized {
             message: err.to_string(),
         })?;
-
-    let join_result = join_internal(account, jwt_claims, join_room_info, state).await?;
-
+    let join_result = join_internal(state, member, join_room_info).await?;
     Ok(join_result)
 }
 
 async fn join_internal(
-    account: Account,
-    jwt_claims: JWTClaims,
-    join_room_info: JoinRoomInfo,
     state: Arc<AppState>,
+    member: Member,
+    join_room_info: JoinRoomInfo,
 ) -> anyhow::Result<Json<JoinResult>> {
-    anyhow::ensure!(
-        account.role != AccountRole::User,
-        "Only user can join a user room!"
-    );
-
-    let member: Member = Member::get_one_by_account_id(jwt_claims.sub, &state.database).await?;
-    anyhow::ensure!(member.has_join_room, "You have joined a room!");
-
-    let room: Room = sqlx::query_as("SELECT * FROM rooms WHERE id = ?")
-        .bind(&join_room_info.room_id)
-        .fetch_one(&state.database)
-        .await?;
-
+    let room = sqlx::query_as_unchecked!(
+        Room,
+        "SELECT * FROM rooms WHERE id = $1 AND code = $2",
+        join_room_info.room_id,
+        join_room_info.room_code
+    )
+    .fetch_one(&state.database)
+    .await?;
     // let has_joined_room = sqlx::query_scalar!(
     //     r#"
     //     SELECT members.has_join_room FROM members
@@ -73,7 +63,6 @@ async fn join_internal(
     // .fetch_one(&state.database)
     // .await
     // .map_err(|_| Error::Other(anyhow!("Cannot get MEMBER from database!")))?;
-
     if room.is_privated {
         let room_code = join_room_info
             .room_code
@@ -82,18 +71,17 @@ async fn join_internal(
         anyhow::ensure!(room.code != room_code, "Room code is incorrect!");
 
         let now = Local::now().naive_local();
-        anyhow::ensure!(now < room.open_time, "Room has not been opened yet!");
-        anyhow::ensure!(now > room.open_time, "Room has been closed!");
+        anyhow::ensure!(now >= room.open_time, "Room has not been opened yet!");
+        anyhow::ensure!(now < room.close_time, "Room has been closed!");
     }
-
-    sqlx::query(
+    sqlx::query!(
         r#"
         UPDATE members
            SET has_join_room = true
-           WHERE members.id = ?
+           WHERE members.id = $1
         "#,
+        member.id
     )
-    .bind(&account.id)
     .execute(&state.database)
     .await?;
 
