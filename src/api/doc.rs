000419f605dce92452af4382cb39c99e75ca0888
LeviathanST <phamquochung170505@gmail.com>
2024-05-15 06:50:14 +0700

    ADD: route scoring/run and submit, room/join

--- src/api/doc.rs
+++ src/api/doc.rs
@@ -1,7 +1,24 @@
 use utoipa::OpenApi;
 
 use super::controller;
+use crate::Error;
+use crate::{enums, util};
 
 #[derive(OpenApi)]
-#[openapi(paths(controller::ping))]
+#[openapi(
+    paths(
+        controller::ping,
+        controller::scoring::run,
+        controller::scoring::submit,
+        controller::room::join,
+    ),
+    security(("api_key" = [])),
+    components(schemas(
+        controller::scoring::Data,
+        controller::room::JoinRoomInfo,
+        enums::ProgrammingLanguage,
+        util::scoring::ExecutionResult,
+        Error,
+    ))
+)]
 pub struct ApiDoc;
