044f6fdcd192a1d80bc2af83bad060b7dd6e0a97
AkagiYuu <khanghuynh0245@gmail.com>
2024-05-18 00:07:12 +0700

    feat: add ErrorResponse

--- src/error.rs
+++ src/error.rs
@@ -5,6 +5,25 @@ use utoipa::ToSchema;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
+#[derive(ToSchema)]
+pub struct ErrorResponse {
+    #[schema(value_type = u16)]
+    status: StatusCode,
+    message: String,
+    details: HashMap<String, String>,
+}
+
+impl IntoResponse for ErrorResponse {
+    fn into_response(self) -> axum::response::Response {
+        Json(json!({
+            "status": self.status.as_u16(),
+            "message": self.message,
+            "details": self.details
+        }))
+        .into_response()
+    }
+}
+
 #[derive(Debug, thiserror::Error, ToSchema)]
 pub enum Error {
     Unauthorized { message: String },
@@ -31,23 +50,28 @@ impl IntoResponse for Error {
                 HashMap::<String, String>::new(),
             ),
         };
-        let response = Json(json!({
-            "status": status.as_u16(),
-            "message": message,
-            "details": details
-        }));
+
+        let response = ErrorResponse {
+            status,
+            message,
+            details,
+        };
         (status, response).into_response()
     }
 }
 
 impl From<axum_extra::typed_header::TypedHeaderRejection> for Error {
     fn from(value: axum_extra::typed_header::TypedHeaderRejection) -> Self {
-        Error::Unauthorized { message: value.to_string() }
+        Error::Unauthorized {
+            message: value.to_string(),
+        }
     }
 }
 impl From<jsonwebtoken::errors::Error> for Error {
     fn from(value: jsonwebtoken::errors::Error) -> Self {
-        Error::Unauthorized { message: value.to_string() }
+        Error::Unauthorized {
+            message: value.to_string(),
+        }
     }
 }
 impl From<anyhow::Error> for Error {
