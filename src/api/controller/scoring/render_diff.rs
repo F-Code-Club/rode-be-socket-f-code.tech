773f078797c13d9172e98eab643e9b7d5d2f628f
Phạm Quốc Hưng <165866405+LeviathanST@users.noreply.github.com>
2024-05-17 21:13:43 +0700

    bugs in api

--- src/api/controller/scoring/render_diff.rs
+++ src/api/controller/scoring/render_diff.rs
@@ -1,6 +1,8 @@
 use std::sync::Arc;
 
 use axum::extract::State;
+use axum::Json;
+use serde::Deserialize;
 use utoipa::ToSchema;
 
 use crate::api::extractor::JWTClaims;
@@ -9,19 +11,19 @@ use crate::database::model::Member;
 use crate::error::{Error, Result};
 use crate::util::scoring::css::render_diff_image;
 
-#[derive(ToSchema)]
+#[derive(Deserialize, ToSchema)]
 pub struct RenderDiffParam {
-    question_image_buffer: &'static [u8],
+    question_image_buffer: Vec<u8>,
     html: String,
 }
 
 #[utoipa::path (
     post,
     tag = "Scoring",
-    path = "/scoring/css",
+    path = "/scoring/render_diff",
     request_body = RenderDiffParam,
     responses (
-        (status = 200, description = "Scoring successfully!",body = ExecutionResult),
+        (status = 200, description = "Scoring successfully!"),
         (status = 400, description = "Bad request!"),
         (status = 401, description = "User's token is not authorized or missed!")
     )
@@ -30,16 +32,19 @@ pub async fn render_diff(
     State(state): State<Arc<AppState>>,
     jwt_claims: JWTClaims,
     render_diff_param: RenderDiffParam,
-) -> Result<(f32, Vec<u8>)> {
+) -> Result<Json<Vec<u8>>> {
     let _ = Member::get_one_by_account_id(jwt_claims.sub, &state.database)
         .await
         .map_err(|err| Error::Unauthorized {
             message: err.to_string(),
         });
 
-    Ok(render_diff_image(
-        render_diff_param.question_image_buffer,
+    let diff_render_image = render_diff_image(
+        &render_diff_param.question_image_buffer,
         render_diff_param.html,
     )
-    .await?)
+    .await?
+    .1;
+
+    Ok(Json(diff_render_image))
 }
