5577504c0896c9bcad119de770cfcde4e7337b01
AkagiYuu <khanghuynh0245@gmail.com>
2024-06-02 20:36:11 +0700

    refactor: move api start out of main

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,4 +1,31 @@
+use std::net::SocketAddr;
+use std::sync::Arc;
+
+use tokio::net::TcpListener;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+use crate::config;
+use crate::AppState;
+
 mod controller;
 mod doc;
 mod extractor;
-pub mod router;
+mod router;
+
+pub async fn start_api() -> anyhow::Result<()> {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    let state = Arc::new(AppState::new().await?);
+    let app = router::build(state);
+    let listener =
+        TcpListener::bind(SocketAddr::new([0, 0, 0, 0].into(), *config::SERVER_PORT)).await?;
+    println!(
+        "R.ODE Socket Is Started And Listening On Port: {}",
+        *config::SERVER_PORT
+    );
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
