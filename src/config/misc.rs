9997bc6627da2e38d245940cb20da3aeff70fc48
AkagiYuu <khanghuynh0245@gmail.com>
2024-06-12 23:47:27 +0700

    feat!: add DATABASE_CRON_JOB_INTERVAL

--- src/config/misc.rs
+++ src/config/misc.rs
@@ -10,3 +10,7 @@ pub static SUBMIT_TIME_OUT: Lazy<u64> = Lazy::new(|| env_or_default("SUBMIT_TIME
 /// Represent the number of test cases to run when the /scoring/run is called
 pub static PUBLIC_TEST_CASE_COUNT: Lazy<usize> =
     Lazy::new(|| env_or_default("PUBLIC_TEST_CASE_COUNT", 2));
+
+// Interval between cron job of database in minute
+pub static DATABASE_CRON_JOB_INTERVAL: Lazy<u64> =
+    Lazy::new(|| env_or_default("DATABASE_CRON_JOB_INTERVAL", 10));
