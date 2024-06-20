abd09accc661fd9ea82bcba0b09cfecc601a5699
AkagiYuu <khanghuynh0245@gmail.com>
2024-06-21 00:10:00 +0700

    fix: java run command

--- src/util/scoring/backend/java.rs
+++ src/util/scoring/backend/java.rs
@@ -2,13 +2,13 @@ use std::str;
 
 pub fn get_compile_command(main_file_name: &str) -> Option<tokio::process::Command> {
     let mut command = tokio::process::Command::new("javac");
-    command.arg(main_file_name);
+    command.arg(format!("{}.java", main_file_name));
 
     Some(command)
 }
 
 pub fn get_execute_command(main_file_name: &str) -> std::process::Command {
-    let mut command = std::process::Command::new("javac");
+    let mut command = std::process::Command::new("java");
     command.arg(main_file_name);
 
     command
