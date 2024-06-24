4581af3caf954aaa50f3e61ca1045f229907b20e
AkagiYuu <khanghuynh0245@gmail.com>
2024-06-24 08:32:30 +0700

    feat!: add env TEMPLATE_PATH

--- src/config/misc.rs
+++ src/config/misc.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use chrono_tz::Tz;
 use once_cell::sync::Lazy;
 
@@ -6,3 +8,6 @@ use super::env_or_default;
 pub const TIME_ZONE: Tz = chrono_tz::Asia::Ho_Chi_Minh;
 
 pub static SUBMIT_TIME_OUT: Lazy<u64> = Lazy::new(|| env_or_default("SUBMIT_TIME_OUT", 5));
+
+pub static TEMPLATE_PATH: Lazy<PathBuf> =
+    Lazy::new(|| env_or_default("TEMPLATE_PATH", PathBuf::from("/tmp/template")));
