2c53a1f0a1e5747952f0eb1eb6a7bb12d7756671
AkagiYuu <khanghuynh0245@gmail.com>
2024-06-24 08:40:19 +0700

    feat: move template download to Template struct

--- src/api/controller/question/get.rs
+++ src/api/controller/question/get.rs
@@ -5,11 +5,10 @@ use axum::{
     Json,
 };
 use serde::{Deserialize, Serialize};
-use tokio::fs;
 use utoipa::{IntoParams, ToSchema};
 use uuid::Uuid;
 
-use crate::{app_state::AppState, config, database::model, util::drive::HubDrive, Result};
+use crate::{app_state::AppState, database::model, Result};
 
 #[derive(Deserialize, IntoParams)]
 pub struct GetQuestionData {
@@ -26,7 +25,7 @@ pub struct QuestionData {
     #[schema(inline)]
     pub test_cases: Vec<model::TestCase>,
     #[schema(format = Binary)]
-    pub question_template: Vec<u8>,
+    pub template_buffer: Vec<u8>,
 }
 
 #[utoipa::path(
@@ -58,18 +57,12 @@ async fn get_internal(
     let template =
         model::Template::get_one_by_question_id(data.question_id, &state.database).await?;
     let test_cases = model::TestCase::get_visible(true, data.question_id, &state.database).await?;
-
-    let template_path = config::TEMPLATE_PATH.join(&template.local_path);
-    if !template_path.exists() {
-        let drive = HubDrive::new().await?;
-        drive.download_file_by_id(&template.url, &template_path).await?;
-    }
-    let question_template = fs::read(&template_path).await?;
+    let template_buffer = template.download().await?;
 
     Ok(Json(QuestionData {
         question,
         template,
         test_cases,
-        question_template,
+        template_buffer,
     }))
 }
