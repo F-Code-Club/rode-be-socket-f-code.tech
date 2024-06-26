8e6a50c35cf3c117fbd4fcd9afd27a6a9a157479
AkagiYuu <khanghuynh0245@gmail.com>
2024-06-26 09:24:33 +0700

    feat!(env): add TEST_CASE_TIME_OUT

--- src/config/misc.rs
+++ src/config/misc.rs
@@ -11,3 +11,6 @@ pub static SUBMIT_TIME_OUT: Lazy<u64> = Lazy::new(|| env_or_default("SUBMIT_TIME
 
 pub static TEMPLATE_PATH: Lazy<PathBuf> =
     Lazy::new(|| env_or_default("TEMPLATE_PATH", PathBuf::from("/tmp/template")));
+
+/// Time out for each test case in second
+pub static TEST_CASE_TIME_OUT: Lazy<u32> = Lazy::new(|| env_or_default("TEST_CASE_TIME_OUT", 3));
