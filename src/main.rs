51139440d7b31b9535962b932c302c5f278acb70
AkagiYuu <khanghuynh0245@gmail.com>
2024-06-28 01:52:56 +0700

    feat: download chrome on launch

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,13 @@ pub mod enums;
 pub mod error;
 pub mod util;
 
+use std::path::Path;
+
+use chromiumoxide::{BrowserFetcher, BrowserFetcherOptions};
 pub use error::{Error, Result};
 
 use app_state::AppState;
+use tokio::fs;
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
@@ -29,6 +33,19 @@ async fn main() {
                 .from_env_lossy(),
         )
         .init();
+
+    let download_path = Path::new("./download");
+    fs::create_dir_all(&download_path).await.unwrap();
+    let fetcher = BrowserFetcher::new(
+        BrowserFetcherOptions::builder()
+            .with_path(download_path)
+            .build()
+            .unwrap(),
+    );
+    let info = fetcher.fetch().await.unwrap();
+    let chrome_path = info.executable_path;
+
+    config::CHROME_PATH.set(chrome_path).unwrap();
 
     let (_, _) = tokio::join!(api::start_server(), api::start_metrics());
 }
